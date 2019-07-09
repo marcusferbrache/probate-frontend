e8d90af77a70a71a361562f5a6145c758921cb65
Luca Del Buono <luca.delbuono@gmail.com>
2019-07-09 16:58:06 +0100

    PRO-3385: Check for webchat FT on all pages but /offline

--- app/featureToggles.js
+++ app/featureToggles.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 const FeatureToggle = require('app/utils/FeatureToggle');
 const featureToggle = new FeatureToggle();
 
-router.get('/*', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'webchat', featureToggle.toggleFeature));
+router.get(/^((?!offline).)*$/gm, (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'webchat', featureToggle.toggleFeature));
 
 router.get('/start-eligibility', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'fees_api', featureToggle.toggleFeature));
 router.get('/copies-uk', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'fees_api', featureToggle.toggleFeature));
