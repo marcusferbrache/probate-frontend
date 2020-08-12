e0e6b31cdf86d49f261a235df3cfe9f0d6389ee7
Linus Norton <linusnorton@gmail.com>
2020-08-12 19:07:03 +0100

    Add payments

--- bin/dev-setup.sh
+++ bin/dev-setup.sh
@@ -42,6 +42,11 @@ do
   sleep 15;
 done
 
+psql -h localhost --username postgres -d fees_register -p 5050 -c "INSERT INTO fee (application_type,channel_type,code,creation_time,event_type,fee_number,fee_type,jurisdiction1,jurisdiction2,keyword,last_updated,service,unspecified_claim_amount) values ('all','default','FEE0288',now(),'miscellaneous',288,'FixedFee','family','probate registry','MNO',now(),'probate',false)";
+psql -h localhost --username postgres -d fees_register -p 5050 -c "INSERT INTO amount (amount_type,creation_time,last_updated) VALUES ('FlatAmount',now(),now())";
+psql -h localhost --username postgres -d fees_register -p 5050 -c "INSERT INTO flat_amount(id, amount) VALUES ((SELECT MAX( id ) FROM amount a ), 3)";
+psql -h localhost --username postgres -d fees_register -p 5050 -c "INSERT INTO fee_version (description,status,valid_from,valid_to,"version",amount_id,fee_id,direction_type,fee_order_name,memo_line,natural_account_code,si_ref_id,statutory_instrument,approved_by,author) VALUES ('Application for the entry or extension of a caveat',1,'2011-04-03 00:00:00.000',NULL,1,(SELECT MAX( id ) FROM amount a ),(SELECT MAX(id) from fee),'cost recovery','Non-Contentious Probate Fees','RECEIPT OF FEES - Family misc probate','4481102173','4','2011 No 588 ','126175','126172')";
+
 # Fees API migrations appear to be broken so it fails to boot first time round
 docker-compose ${COMPOSE_FILE} restart fees-api
 
