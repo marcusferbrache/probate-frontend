cac415e920d72d0892e0b10a75d912c33b593da4
PhaniP <phani.perla@hmcts.net>
2020-10-19 19:05:58 +0100

    DTSPB-744 CSS reference updated

--- test/end-to-end/pages/pcq/pcqstartpage.js
+++ test/end-to-end/pages/pcq/pcqstartpage.js
@@ -2,7 +2,7 @@
 
 module.exports = function() {
     const I = this;
-    I.click({css: '.govuk-button.govuk-button--secondary'});
+    I.click('#main-content > div > div > form > ul > li:nth-child(2) > button');
     I.wait(3);
     console.log('PCQ completed');
 };
